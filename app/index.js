cbea694ccf4fcbeba5c103119abaeb5b3259c4e9
Jeff Jenkins <jenkinsjeffrey@gmail.com>
2014-01-27 17:30:51 -0800

    fire form submit.

---
+++ app/index.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var fs = require('fs');
+var index = fs.readFileSync('index.html');
+
+http.createServer(function(req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+    res.end(index);
+}).listen(9615);
