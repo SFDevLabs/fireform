b7f86cc09122ea81cd56ca63bedc082afeb6aeaa
jeff jenkins <jenkinsjeffrey@gmail.com>
2014-02-08 21:19:52 -0800

    new modal.

--- app/js/directives.js
+++ app/js/directives.js
@@ -19,5 +19,29 @@ angular.module("myApp.directives", [])
             }
         };
     })
+     .directive("bodyClassInjectClick", function($rootScope) {
+        return {
+            restrict: 'A',
+            link: function(scope, el, attr){
+
+                scope.stuff=function(){
+                    $rootScope.bodyClass=attr.bodyClassInjectClick
+                };
+
+                el.bind('click', function(){
+                    scope.stuff();
+                });
+//                $rootScope.bodyClass=attr.bodyClassInject
+            }
+        };
+    })
+    .directive("bodyClassInject", function($rootScope) {
+        return {
+            restrict: 'A',
+            link: function(scope, el, attr){
+                $rootScope.bodyClass=attr.bodyClassInject
+            }
+        };
+    })
 
     //make directive to invoke modal
