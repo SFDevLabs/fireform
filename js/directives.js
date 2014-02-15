82b98c9452346de6c5f4c72ba15cf823fdba0fa7
jeff jenkins <jenkinsjeffrey@gmail.com>
2014-02-15 05:15:09 -0800

    adding syntax highlights

--- js/directives.js
+++ js/directives.js
@@ -39,6 +39,7 @@ angular.module("myApp.directives", [])
                 var rowsHeader = scope.$parent.rowsHeader,
                     rows=scope.$parent.rows,
                     valueF=scope.valueF;
+                    scope.$parent.predicate="-_time", //This sets the default sort to most recent
                     valueF._time=new Date(scope.valueF._time); //Make our a date a time object
 
                 scope.$parent.rowsHeader = _.union(//union (join with no duplicates) the new keys with the building list
@@ -64,4 +65,18 @@ angular.module("myApp.directives", [])
             }
         };
     })
+
+.directive("highlight", function($rootScope) {
+        return {
+            restrict: 'A',
+            link: function(scope, el, attr){
+                debugger
+
+                var m=el.html().replace("{{location}}",scope.$location.absUrl())
+                el.html(m)
+                hljs.highlightBlock(el[0])
+            }
+        };
+    })
+
 
