5fae79dd5f67ac7dc39a05b15adedc1625056d45
jeff jenkins <jenkinsjeffrey@gmail.com>
2014-02-17 01:25:40 -0800

    adding footer partial and replace.

--- js/directives.js
+++ js/directives.js
@@ -121,4 +121,13 @@ angular.module("myApp.directives", [])
                     scope.mostRecent=""
             }
         };
-    });
+    })
+.directive('includeReplace', function () {
+    return {
+        require: 'ngInclude',
+        restrict: 'A', /* optional */
+        link: function (scope, el, attrs) {
+            el.replaceWith(el.children());
+        }
+    };
+});
